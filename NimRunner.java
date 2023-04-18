fb66ec74c6358a78abd1114a94676f8c49b57dbf
Your Name <cassidyrosemckee@gmail.com>
2023-04-18 08:58:27 -0400

    Adding nimRunner file to git repository

---
+++ NimRunner.java
@@ -0,0 +1,14 @@
+public class NimRunner{
+    public static void main (String[] args){
+
+    }
+    public static void runGame(){
+
+    }
+    public static int getXMove(){
+
+    }
+    public static int getYMove(){
+
+    }
+}
