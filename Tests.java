7387c0720ff40eb3ebee4e2f3f3333df20ff7af6
Your Name <cassidyrosemckee@gmail.com>
2023-04-24 07:57:17 -0400

    Made all tests return true if they are correct and all working for minimax function

--- Tests.java
+++ Tests.java
@@ -1,12 +1,15 @@
 public class Tests{
 
     public static void main(String[] args){
+        //Testing minimax function
         System.out.println("Testing my NimRunner class.");
-        System.out.println(NimRunner.miniMax(1,true)==-1); //this should print out true if i'm getting -1
-        System.out.println(NimRunner.miniMax(1,false)==-1); //this should print out true if it's working (im getting 1)
-        System.out.println(NimRunner.miniMax(0,true)==1);//edge case I won should return 1
-        System.out.println(NimRunner.miniMax(0,false)==-1);//edge case I lost should return -1
+        System.out.println(NimRunner.minimax(1,true)==-1); //this should print out true if i'm getting -1
+        System.out.println(NimRunner.minimax(1,false)==1); //this should print out true if it's working (im getting 1)
+        System.out.println(NimRunner.minimax(0,true)==1);//edge case I won should return 1
+        System.out.println(NimRunner.minimax(4,true)==1);//should return 1 true 
 
+        //Testing bestMove function 
+        
     }
 
 }
